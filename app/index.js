b66e8c53034436ea6f11b87fc79241366d11a5a2
AlexMog <alexmog@live.fr>
2018-01-31 23:07:03 +0100

    Init

---
+++ app/index.js
@@ -0,0 +1,52 @@
+let lotion = require('lotion')
+let coins = require('coins')
+let OracleTx = require('./oracle-tx.js')
+let ValidatorReward = require('./validator-reward.js')
+//let config = require('../config.js')
+
+const oneTIB = 1e8
+
+module.exports = function (opts = {}) {
+  let app = lotion({
+    // default options
+    p2pPort: 46658,
+    tendermintPort: 46657,
+      genesis: require.resolve('../genesis.json'),
+      peers: config.peers.map((addr) => `${addr}:46658`),
+
+    // inherit properties from `opts`
+    ...opts
+  })
+
+  // create cryptocurrency
+  app.use(coins({
+    handlers: {
+      oracleTx: OracleTx({
+        oraclePubkey: '',
+        foundersAddress: '',
+        foundersPercent: 1
+      })
+    }
+  }))
+
+  // enforce fee rules
+  app.use(function (state, tx) {
+    // no fees for oracle grants
+    if (tx.from[0].type === 'oracleTx') return
+
+    if (tx.to[0].type !== 'fee') {
+      throw Error('First output must pay fee')
+    }
+    if (tx.to[0].amount !== 0.01 * oneTIB) {
+      throw Error('Fee must be 0.01 TIB')
+    }
+  })
+
+  // pay rewards to validators
+  app.use(ValidatorReward({
+    perValidatorPerBlock: 0.0016 * oneTIB
+  }))
+
+  let lotionPort = opts.lotionPort || 3000
+  return app.listen(lotionPort)
+}
---
+++ app/validator-reward.js
@@ -0,0 +1,25 @@
+let { addressHash } = require('coins/src/common.js')
+
+module.exports = function ({ perValidatorPerBlock }) {
+  return {
+    // this is a block handler, run it each time a block is made
+    type: 'block',
+    middleware (state, chainInfo) {
+      for (let pubkey in chainInfo.validators) {
+        // remove first byte because that just tells us the type
+        let pubkeyBuf = Buffer.from(pubkey, 'hex').slice(1)
+        let address = addressHash(pubkeyBuf)
+
+        // add to this validator's address
+        if (!state.accounts[address]) {
+          state.accounts[address] = {
+            balance: 0,
+            sequence: 0
+          }
+        }
+        state.accounts[address].balance += perValidatorPerBlock
+        console.log("New block made! Validator " + address + " balance: " + (state.accounts[address].balance / 1e8));
+      }
+    }
+  }
+}
